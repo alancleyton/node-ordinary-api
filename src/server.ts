15db6e685fc6ed1892464d1d4d5b8cec688fb82c
Alan Cleyton <alancleyton67@gmail.com>
2023-10-03 14:34:32 -0300

    feat: create an node http server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { createServer } from 'http'
+
+const server = createServer((request, response) => {
+  response.end('Hello world!')
+})
+
+server.listen(3000, () => {
+  console.log('server is running on http://localhost:3000')
+})
