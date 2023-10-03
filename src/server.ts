629bd6331ef3c820a4fe5f50666fa3c17a79a3f3
Alan Cleyton <alancleyton67@gmail.com>
2023-10-03 14:58:03 -0300

    lint: add ESlint and Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { createServer } from 'http'
+import { createServer } from 'http';
 
 const server = createServer((request, response) => {
-  response.end('Hello world!')
-})
+  response.end('Hello world!');
+});
 
 server.listen(3000, () => {
-  console.log('server is running on http://localhost:3000')
-})
+  console.log('server is running on http://localhost:3000');
+});
